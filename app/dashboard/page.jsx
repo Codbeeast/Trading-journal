b6b19fec3626669ac3d4dd5b0a10bd999016fe30
Priyanshu <vishwakarmapriyanshu9099@gmail.com>
2025-08-04 16:19:27 +0530

    Update page.jsx

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -218,7 +218,7 @@ const TradingDashboard = () => {
         </section>
 
         {/* Quarterly Tables */}
-        <section className="mb-8">
+        <section className="">
           <QuaterlyTables />
         </section>
       </div>

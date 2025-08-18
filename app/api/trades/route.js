6733f3faee2882c22a3a5e3e2e590057d18f3f6e
Codbeeast <vishwakarmapriyanshu9099@gmail.com>
2025-08-18 11:37:09 +0530

    Corrected the db import

--- app/api/trades/route.js
+++ app/api/trades/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import dbConnect from '@/lib/dbConnect';
+import dbConnect from '@/lib/db';
 import Trade from '@/models/Trade';
 import Strategy from '@/models/Strategy';
 

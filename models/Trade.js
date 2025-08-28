16ac07ec46964c0753fc810ef3456f648853a034
Abinash Das <abinash9731@gmail.com>
2025-08-28 22:13:32 +0530

    trade schema improved

--- models/Trade.js
+++ models/Trade.js
@@ -133,6 +133,18 @@ const TradeSchema = new Schema(
       max: 10,
       default: 5
     },
+    patience: {
+      type: Number,
+      min: 1,
+      max: 10,
+      default: 5
+    },
+    confidence: {
+      type: Number,
+      min: 1,
+      max: 10,
+      default: 5
+    },
   },
   { timestamps: true }
 );
